aac4de0b7c3c94569afc3e5a7fa835f1196e5546
monet <monet@monet.com>
2021-12-13 15:59:36 +0800

    second commit

--- src/main/java/com/monet/Poker.java
+++ src/main/java/com/monet/Poker.java
@@ -5,6 +5,6 @@ import java.util.ArrayList;
 public class Poker {
     public static void main(String[] args) {
         ArrayList<Object> poker = new ArrayList<>();
-
+        System.out.println("haha");
     }
 }
