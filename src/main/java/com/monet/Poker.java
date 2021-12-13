6a8d1ed40aab29d9c8c29ce906789865edae83cb
monet <monet@monet.com>
2021-12-13 17:39:35 +0800

    hot-fix commit4

--- src/main/java/com/monet/Poker.java
+++ src/main/java/com/monet/Poker.java
@@ -5,12 +5,6 @@ import java.util.ArrayList;
 public class Poker {
     public static void main(String[] args) {
         ArrayList<Object> poker = new ArrayList<>();
-        System.out.println("haha");
-        System.out.println("hahahahahaha");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("hot-fix test");
-        System.out.println("hahha");
-        System.out.println("123");
+
     }
 }
